3abfc0dfdaebfd24296def2139a10963152922a7
Rick Hallihan <rick.hallihan@gmail.com>
2021-06-19 20:30:01 -0400

    basic

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  var display="";
+  var display="<pre>";
   for (const row of game) { 
     for (const column of row) {
       display += column + "|"
       }
-    display += "------";
+    display += "\n------";
   }
+  display+="</pre>"
   res.write(display);
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
