ab09af58975e8d9497e2383cea0ada02c12f4071
Ryan H. Code <42245712+ryanhcode@users.noreply.github.com>
2021-06-20 12:15:47 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require('path');
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 80;
 const app = express();
 var cors = require('cors');
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(cors());
 
     
