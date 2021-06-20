a46d397bc0564a64ec42aa0bbf4cfb7ff7b0d805
Ryan H. Code <42245712+ryanhcode@users.noreply.github.com>
2021-06-20 11:55:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,99 @@
-var game = [[" "," "," "],[" "," "," "],[" "," "," "]];
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
-var http = require('http');
+const port = process.env.PORT || 4001;
+const app = express();
+var cors = require('cors');
 
-//create a server object:
-http.createServer(function (req, res) {
-  var display="";
-  for (const row of game) { 
-    for (const column of row) {
-      display += column + "|"
-      }
-    display += "<br/>------";
-  }
-  display+="<br>The only way to win is not to play"
-  res.write(display);
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+app.use(cors());
+
+    
+const server = http.createServer(app, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+var clients = []
+
+var data = {
+    personTurn: false,
+    time: 20,
+    board: [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ],
+    results: [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ]
+}
+JSON.safeStringify = (obj, indent = 2) => {
+    let cache = [];
+    const retVal = JSON.stringify(
+      obj,
+      (key, value) =>
+        typeof value === "object" && value !== null
+          ? cache.includes(value)
+            ? undefined // Duplicate reference found, discard key
+            : cache.push(value) && value // Store value in our collection
+          : value,
+      indent
+    );
+    cache = null;
+    return retVal;
+  };
+
+const io = socketIo(server); // < Interesting!
+io.on("connection", (socket) => {
+    socket.send(JSON.stringify(data))
+    clients.push(socket)
+    socket.on("message", (m)=> {
+        var d = JSON.parse(m)
+        data.board[d.vote]++
+        if(d.adminVote) {
+            data.results[d.adminVote] = 2
+            data.personTurn = false
+            data.time = 10
+        }
+    })
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
+}
+
+setInterval(()=>{
+    if(data.time > 0) data.time-=1
+    if(data.time == 0 && !data.personTurn) { 
+        data.personTurn = true
+        data.results[indexOfMax(data.board)]=1
+        data.board = [0,0,0,0,0,0,0,0,0]
+    } 
+    clients.forEach((socket)=>{
+        socket.send(JSON.stringify(data))
+    })
+}, 1000)
