0d1651abb2f4a931ad37dded22452785b5798a10
Rick Hallihan <rick.hallihan@gmail.com>
2021-06-19 20:21:23 -0400

    game

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var game = [["","",""],["","",""],["","",""]];
+var game = [[" "," "," "],[" "," "," "],[" "," "," "]];
 
 var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  var display="";
+  for (const row of game) { 
+    for (const column of row) {
+      display += column + "|"
+      }
+    display += "------";
+  }
+  res.write(display);
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
